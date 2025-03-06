2647f18e3abd2e9370fac8c47ebd29a3e6714f46
JeissonHrdz <jeissonhrdz186@gmail.com>
2025-03-06 15:39:52 -0500

    ADD: Insert,Update and Delete expense functions

--- src/main/java/Model/DAO/ExpenseDAO.java
+++ src/main/java/Model/DAO/ExpenseDAO.java
@@ -1,43 +1,96 @@
 package Model.DAO;
+
 import Model.VO.ExpenseVO;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
 public class ExpenseDAO {
 
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    Path path = Paths.get("src/main/java/DataBase/expense.json");
+
     public boolean insert(ExpenseVO expense) {
+        boolean result = false;
+        String json = "";
         try {
-
-            Date date = new Date();
-            String str = new SimpleDateFormat("yyyy-MM-dd").format(date);
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            ExpenseVO expenseVO = new ExpenseVO(1, "Prueba", 5350, "Food", str);
-            Writer writer = Files.newBufferedWriter(Paths.get("src/main/java/DataBase/expense.json"));
-            gson.toJson(expenseVO, writer);
-            writer.close();
-
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-
+            List<ExpenseVO> expenses = processJSON();
+            if (!Objects.isNull(expenses)) {
+                expenses.add(expense);
+                json = gson.toJson(expenses);
+            } else {
+                json = "[" + gson.toJson(expense) + "]";
+            }
+            Files.write(path, json.getBytes());
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return false;
+        return result;
     }
 
     public boolean update(ExpenseVO expense) {
-        return false;
+        boolean result = false;
+
+        try {
+            List<ExpenseVO> expenses = processJSON();
+            for (ExpenseVO expenseVO : expenses) {
+                if (expenseVO.getId() == expense.getId()) {
+                    expenseVO.setCategory(expense.getCategory());
+                    expenseVO.setDescription(expense.getDescription());
+                    expenseVO.setDate(expense.getDate());
+                    expenseVO.setAmount(expense.getAmount());
+
+                    String json = gson.toJson(expenses);
+                    Files.write(path, json.getBytes());
+                    result = true;
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
-    public boolean delete(ExpenseVO expense) {
-        return false;
+    public ExpenseVO getExpenseById(int id) {
+        ExpenseVO expense = null;
+        try {
+            List<ExpenseVO> expenses = processJSON();
+            for (ExpenseVO expenseObj : expenses) {
+                if (expenseObj.getId() == id) {
+                    expense = expenseObj;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return expense;
+    }
+
+    public boolean delete(int id) {
+        boolean result = false;
+        try {
+            List<ExpenseVO> expenses = processJSON();
+            expenses.removeIf(expenseVO -> expenseVO.getId() == id);
+            String json = gson.toJson(expenses);
+            Files.write(path, json.getBytes());
+            result = true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     public ArrayList<ExpenseVO> selectAll() {
@@ -46,6 +99,13 @@ public class ExpenseDAO {
 
     public ArrayList<ExpenseVO> selectByCategory(String category) {
         return null;
+    }
+
+    public List<ExpenseVO> processJSON() throws FileNotFoundException {
+        BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(path)));
+        Type expenseList = new TypeToken<List<ExpenseVO>>() {
+        }.getType();
+        return gson.fromJson(reader, expenseList);
     }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,31 @@
 package org.example;
 
 
+import Model.DAO.ExpenseDAO;
+import Model.VO.ExpenseVO;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+       /* ExpenseDAO dao = new ExpenseDAO();
+        Date date = new Date();
+        String str = new SimpleDateFormat("yyyy-MM-dd").format(date);
+        dao.insert(new ExpenseVO(3,"prueba 3",5230,"rent",str));*/
 
+        ExpenseDAO expenseDAO = new ExpenseDAO();
+     /*   ExpenseVO expense = expenseDAO.getExpenseById(8);
+        if(expense != null) {
+            expense.setAmount(expense.getAmount() + expense.getAmount());
+            expense.setDescription("Expense Description");
+            expense.setCategory("Expense Category");
+            expenseDAO.update(expense);
+        } else {
+            System.out.println("Expense not found");
+        }*/
+
+        expenseDAO.delete(5);
 
 
     }
